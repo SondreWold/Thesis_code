6243a1cd5596a8873959206c6b4993bfdfa067a2
sondre <sondrewold1@gmail.com>
2021-12-03 10:58:11 +0100

    Add Hugginface code for common sense qa datasets

---
+++ run_qa.sh
@@ -0,0 +1,14 @@
+MODEL="./models/mlm_100k_cased" #No trailing / !!
+TOKENIZER="bert-base-cased"
+DATASET_NAME=swag
+
+python src/run_common_sense_qa.py \
+  --model_name_or_path $MODEL \
+  --tokenizer_name $TOKENIZER \
+  --dataset_name $DATASET_NAME \
+  --max_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --tune_both True \
+  --output_dir /tmp/$DATASET_NAME/
